2a7c9f582992cd4827109b642e65a21977074da0
Aaron Bach <aaron@fusionbox.com>
2010-04-01 10:01:51 -0600

    Modified fn_exists to be a bit more verbose

--- zsh.d/functions.d/fn_exists.sh
+++ zsh.d/functions.d/fn_exists.sh
@@ -4,5 +4,10 @@
 #-------------------------------------------------------
 fn_exists()
 {
-  type $1 | grep -q 'shell'
+	if [ "$#" -ne 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: fn_exists [function-name]" >&2
+    return 1
+  fi
+
+  type $1 &>/dev/null && echo "${1}() found" || echo "${1}() not found"
 }
