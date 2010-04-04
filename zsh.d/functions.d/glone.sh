c789b740cbc915ef8d7388dedeb4e99b8b986b60
Aaron Bach <bachya1208@googlemail.com>
2010-04-04 12:47:41 -0600

    Modified glone to accept 1 arg (URL to git repo) or the original 2 arg formats

--- zsh.d/functions.d/glone.sh
+++ zsh.d/functions.d/glone.sh
@@ -3,10 +3,14 @@
 #--------------------------------------------------
 glone()
 {
-  if [ "$#" -ne 2 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  if [ "$#" -gt 2 ] || [ "$#" -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo "Usage: glone [host] [repo-name]" >&2
     return 1
   fi
  
-  git clone git@${1}:${2}
+  if [ "$#" -eq 1 ]; then
+    git clone ${1}
+  else  
+    git clone git@${1}:${2}
+  fi
 }
