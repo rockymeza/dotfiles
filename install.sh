df0ee0d32530b9727b056bb98bd183096c1fa43e
Rocky Meza <rockymeza@gmail.com>
2010-08-10 23:06:46 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -18,9 +18,9 @@ gitconfig_setup()
   
   if [ -a ${HOME}/.gitconfig ]; then
     rm ${HOME}/.gitconfig
-    cp ${PWD}/gitconfig ${HOME}/.gitconfig
+    cp ${PWD}/git/gitconfig ${HOME}/.gitconfig
   else
-    cp ${PWD}/gitconfig ${HOME}/.gitconfig
+    cp ${PWD}/git/gitconfig ${HOME}/.gitconfig
   fi
   
   read -p "Git Name (leave blank to default to '${name}'): " new_name
@@ -38,7 +38,7 @@ gitconfig_setup()
     git config --global user.email "${new_email}"
   fi
   
-  git config --global core.excludesfile ~/.gitignore
+  git config --global core.excludesfile $PWD/git/gitignore
 }
 
 #--------------------------------------------------
@@ -52,77 +52,28 @@ help_message()
 }
 
 #--------------------------------------------------
-#   Function to determine whether a given entry
-#   exists in an array
-#--------------------------------------------------
-in_array()
-{
-  haystack=( "$@" )
-  haystack_size=( "${#haystack[@]}" )
-  needle=${haystack[$((${haystack_size}-1))]}
-  for ((i=0;i<$(($haystack_size-1));i++)); do
-    h=${haystack[${i}]};
-    [ $h = $needle ] && return 0
-  done
-}
-
-#--------------------------------------------------
 #   Creates a symbolically linked "." version of a
 #   file in the user's home directory
 #--------------------------------------------------
 link_file()
 {
+  if [[ -a ${HOME}/.${1} ]]; then
+    echo "Removing old ~/.${1}..."
+    rm ${HOME}/.${1}
+  fi
+
   echo "Linking ~/.${1}..."
   ln -s ${PWD}/${1} ${HOME}/.${1}
 }
 
 #--------------------------------------------------
-#   Replaces a "." symlink in the user's home 
-#   directory
-#--------------------------------------------------
-replace_file()
-{
-  echo "Removing old ~/.${1}..."
-  rm ${HOME}/.${1}
-  link_file $1
-}
-
-#--------------------------------------------------
 #   Main program
 #--------------------------------------------------
-if [ $# -ne 1 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
-  help_message
-else
-  case $1 in
-    install)
-      replace_all=false
-      copied_files=(gitignore zshrc)
-      for i in *
-      do
-        file=$i
-        [[ $file =~ .*~$ ]] && continue
-        in_array "${copied_files[@]}" "$i"
-        if [ $? -eq 0 ]; then
-          if [ -a ${HOME}/.${file} ]; then
-            if $replace_all; then
-              replace_file $file
-            else
-              read -p "Overwrite ~/.${file}? [yNaq]: " response
-              case $response in
-                a) replace_all=true
-                   replace_file $file;;
-                y) replace_file $file;;
-                q) exit;;
-                *) echo "Skipping ~/.${file}";;
-              esac
-            fi
-          else
-            link_file $file
-          fi
-        fi
-      done;
-      gitconfig_setup;;
-    gc) gitconfig_setup;;
-    *)  help_message;;
-  esac
-fi
+link_file zshrc
+link_file vimrc
+link_file vim
+link_file Xdefaults
+link_file xinitrc
+link_file xsession
+
+gitconfig_setup
