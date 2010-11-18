e83af7c69a962c3b4804096ae4f7e056c5f88fff
Rocky Meza <rockymeza@gmail.com>
2010-11-18 09:50:44 -0700

    timeout in trying to get headers

--- autoload/o.sh
+++ autoload/o.sh
@@ -14,21 +14,22 @@ o()
       type=$(file -b --mime-type $1 | sed 's/^\([a-z]\+\)\/.*$/\1/')
       if [[ "$type" == "text" ]]; then
         if [[ ! -z $EDITOR ]]; then
-          $EDITOR $1
+          return $EDITOR $1
         else
-          $PAGER $1
+          return $PAGER $1
         fi
       fi
     else
-      curl_response=$(curl -I -s $1 | head -n 1 | sed 's/^.*\([0-9]\{3\}\).*$/\1/')
+      curl_response=$(curl -I -s -m 1 $1 | head -n 1 | sed 's/^.*\([0-9]\{3\}\).*$/\1/')
       if [[ "$curl_response" == "200" ]] || [[ "$curl_response" == "301" ]]; then # returns a successful HTTP status
         if [[ ! -z $BROWSER ]]; then 
-          $BROWSER $1
+          return $BROWSER $1
         else
-          /usr/bin/x-www-browser $1
+          return /usr/bin/x-www-browser $1
         fi
       else
         echo "I don't know what to do" # give up
+        return 1
       fi
     fi
   fi
@@ -41,7 +42,7 @@ o, opens things
 Tries to figure out what something is and than opens it.
 
 Usage:
-      o path/to/file/or/directory/or/url
+      o path/to/file/or/directory/or/URL
 
 Options:
       -h, --help            Show this help screen
