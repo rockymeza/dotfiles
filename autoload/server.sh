a76af7240f5959fec4f2f920f2c201b7ce1de29f
Rocky Meza <rocky@fusionbox.com>
2012-07-11 15:25:10 -0600

    server now needs no parameters, also fixed the port thing

--- autoload/server.sh
+++ autoload/server.sh
@@ -2,10 +2,13 @@
 # Google Chrome.  Based on what I saw Paul Irish do.
 function server()
 {
+  [ $1 ] && dir=$1 || dir='.'
   [ $2 ] && port=$2 || port=8000
 
-  cd $1
+  cd $dir
   (sleep 1 && google-chrome "http://localhost:$port")
-  python -m SimpleHTTPServer
+  # if I were on mac, I would do this instead
+  # (sleep 1 && open "http://localhost:$port")
+  python -m SimpleHTTPServer $port
   cd -
 }
