84696fef05da8f876a5bc659c6346422dee7a656
Rocky Meza <rocky@fusionbox.com>
2013-03-12 10:43:00 -0600

    added b_hooks by colin

--- autoload/b.sh
+++ autoload/b.sh
@@ -1,18 +1,119 @@
-#!/usr/bin/env bash
-#----------------------------------------------
-#    Bookmarking for  directories
-#----------------------------------------------
-BOOKMARKS_FILE=$HOME/.b_bookmarks
+# b, a simple bookmarking system
+# by Rocky Meza
 
-if [[ ! -f "$BOOKMARKS_FILE" ]]; then
-  touch "$BOOKMARKS_FILE"
+BOOKMARKS_FILE=$HOME/.b_bookmarks
+read -r -d '' USAGE <<HEREDOC
+b, a simple bookmarking system
+
+Usage:
+      b [bookmark] [directory]
+      b [bookmark] [file]
+
+Options:
+      -h, --help            Show this help screen
+
+Notes:
+      If b is run with no arguments, it will list all of the bookmarks.
+      If it is given a bookmark that is a directory, it will attempt to cd into that bookmark.
+      If it is given a bookmark that is a file, it will attempt to open that bookmark.
+      If it is given a bookmark and directory or file, it will create that bookmark.
+
+Examples:
+    $ b home /home/user
+      Added home,/home/user to bookmark list
+    $ b p /home/user/.profile
+      Added p,/home/user/.profile to bookmark list
+    $ b
+      List of bookmarks:
+      home,/home/user
+      p,/home/user/.profile
+      ...
+    $ b home
+      will cd to the home directory
+    $ echo \`b home\`
+      /home/user
+    $ b p
+      will open ~/.profile
+      If your computer is a Mac, it will use the \`open\` command, otherwise it
+      will use the \`\$EDITOR\`.
+HEREDOC
+
+## private
+
+# Creates the bookmark database if it doesn't exist.
+__b_init()
+{
+  if [[ ! -f "$BOOKMARKS_FILE" ]]; then
+    touch $BOOKMARKS_FILE
+  fi
+}
+
+# List all of the bookmarks in the database.
+__b_list()
+{
+  echo "List of bookmarks:"
+  # sorry
+  cat "$BOOKMARKS_FILE"
+}
+
+# Will add a bookmark to the database if it doesn't already exist.  `add` will
+# also expand the bookmark.  You can use relative paths or things like `.`,
+# `..`, and `~`.
+__b_add()
+{
+  __b_find_mark $1
+  if [[ -n "$mark" ]]; then
+    echo "That bookmark is already in use."
+  else
+    dir=`readlink -f $2`
+
+    echo "$1,$dir" >> $BOOKMARKS_FILE
+    echo "Added $1,$dir to bookmarks list"
+  fi
+}
+
+# Will `cd` to the bookmarked directory.  If no bookmark matches the one
+# specified, it will print an error.
+open_command="$EDITOR"
+if [[ `uname` = "Darwin" ]]; then
+  open_command=open
 fi
 
+__b_cd()
+{
+  __b_find_mark "$1"
+  if [[ -n "$mark" ]]; then
+    dir=$(echo $mark | sed 's/^[^,]*,\(.*\)/\1/')
+    # if not a tty, print to stdout
+    if [ ! -t 1 ] ; then
+      echo -n "$dir"
+    elif [[ -d $dir ]]; then
+      cd $dir
+      if [[ -f "$dir/.b_hook" ]]; then
+        source "$dir/.b_hook"
+      fi
+    else
+      $open_command "$dir"
+    fi
+  else
+    echo "That bookmark does not exist." >&2
+  fi
+}
+
+__b_find_mark()
+{
+  mark=$(grep "^$1," < $BOOKMARKS_FILE)
+}
+
+## public
+
+# This is the entry point.  It parses the arguments and then delegates to other
+# functions.
 b()
 {
   if [[ "$#" -eq 1 ]]; then
     if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
-      __b_usage
+      echo "$USAGE"
     else
       __b_cd $1
     fi
@@ -23,71 +124,5 @@ b()
   fi
 }
 
-__b_list()
-{
-  if [[ -s "$BOOKMARKS_FILE" ]]; then
-    echo "List of bookmarks:"
-    cat "$BOOKMARKS_FILE"
-  else
-    echo "You have not set any bookmarks."
-  fi
-}
-
-__b_add()
-{
-  __b_find_mark $1
-  if [[ -n "$__b_mark" ]]; then
-    echo "That bookmark is already in use."
-  else
-    dir=`readlink -f $2`
-
-    echo "$1,$dir" >> $BOOKMARKS_FILE
-    echo "Added $1,$dir to bookmarks list"
-  fi
-
-}
-
-__b_cd()
-{
-  __b_find_mark $1
-  if [[ -n "$__b_mark" ]]; then
-    dir=$(echo $__b_mark | sed 's/^[^,]*,\(.*\)/\1/')
-    cd $dir
-  else
-    echo "That bookmark does not exist."
-  fi
-}
-
-__b_find_mark()
-{
-  __b_mark=$(cat "$BOOKMARKS_FILE" | grep "^$1,")
-}
-
-__b_usage()
-{
-  cat <<HEREDOC
-b, a simple bookmarking system
-
-Usage:
-      b [options] [bookmark] [directory]
-
-Options:
-      -h, --help            Show this help screen
-
-Notes:
-      If b is run with no arguments, it will list all of the bookmarks.
-      If it is given a bookmark, it will attempt to cd into that bookmark.
-      If it is given a bookmark and directory, it will create that bookmark.
-
-Examples:
-    $ b
-      List of bookmarks:
-      home,/home/user
-      ..
-    $ b home
-      will cd to the home directory
-    $ b home /home/user
-      Added home,/home/user to bookmark list    
-HEREDOC
-}
-
+# main
+__b_init
